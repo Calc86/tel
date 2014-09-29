bf2d30a5f6fa7ab75ab91c3222feb28114d1f126
calc <calc@list.ru>
2014-09-29 12:29:18 +0400

    redirect to org view

--- protected/controllers/OrgIpController.php
+++ protected/controllers/OrgIpController.php
@@ -115,11 +115,14 @@ class OrgIpController extends Controller
 	 */
 	public function actionDelete($id)
 	{
-		$this->loadModel($id)->delete();
+        $model = $this->loadModel($id);
+        $oid = $model->org_id;
+		$model->delete();
+
 
 		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('users/index', 'id' => $oid));
 	}
 
 	/**
